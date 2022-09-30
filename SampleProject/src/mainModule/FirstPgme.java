e995ef2cce39343efda58cb858fd68725ec9e417
shaun <shaun@CP>
2022-09-30 13:09:37 +0530

    first commit

---
+++ SampleProject/src/mainModule/FirstPgme.java
@@ -0,0 +1,11 @@
+package mainModule;
+
+
+public class FirstPgme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Weocome to Java");
+	}
+
+}
---
+++ SampleProject/src/mainModule/GettersClass.java
@@ -0,0 +1,37 @@
+package mainModule;
+
+public class GettersClass {
+
+	private int marks;
+	private String str;
+	private Float number;
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	public String getStr() {
+		return str;
+		
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+		
+	}
+
+	public Float getNumber() {
+		return number;
+		
+	}
+
+	public void setNumber(Float number) {
+		this.number = number;
+		
+	}
+	
+}
